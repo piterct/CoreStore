6c2fd731fb7871ed0e2cd16df5dcbcd7d345f1e9
Michael Peter <michael_piterct@hotmail.com>
2020-01-18 01:50:23 -0300

    #9

---
+++ CoreStore.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CoreStore.Infra
+{
+    public class Class1
+    {
+    }
+}
---
+++ CoreStore.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CoreStore.Shared
+{
+    public class Class1
+    {
+    }
+}
