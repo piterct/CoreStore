55d13ba05fa00d9e1657dcf2ac470701e608c18b
Michael Peter <michael_piterct@hotmail.com>
2020-01-20 19:48:20 -0300

    #14  Collections

--- CoreStore.Domain/StoredContext/Entities/Customer.cs
+++ CoreStore.Domain/StoredContext/Entities/Customer.cs
@@ -1,10 +1,12 @@
 ﻿using CoreStore.Domain.StoredContext.ValueObjects;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CoreStore.Domain.StoredContext.Entities
 {
     public class Customer
     {
+        private readonly IList<Address> _addresses;
         public Customer(Name name,
             Document document,
             Email email,
@@ -15,15 +17,19 @@ namespace CoreStore.Domain.StoredContext.Entities
             Document = document;
             Email = email;
             Phone = phone;
-            Addresses = new List<Address>();
+            _addresses = new List<Address>();
         }
 
         public Name Name { get; set; }
         public Document Document { get; private set; }
         public Email Email { get; private set; }
         public string Phone { get; private set; }
-        public IReadOnlyCollection<Address> Addresses { get; private set; }
+        public IReadOnlyCollection<Address> Addresses => _addresses.ToArray();
 
+        public void AddAdress(Address address)
+        {
+            _addresses.Add(address);
+        }
         public override string ToString()
         {
             return Name.ToString();
