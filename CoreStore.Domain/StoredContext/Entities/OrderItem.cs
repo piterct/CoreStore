50315901e21fbd57db8efefc1efa5c5333a4a038
Michael Peter <michael_piterct@hotmail.com>
2020-01-20 12:00:03 -0300

    #11  Anti-Corruption Code

--- CoreStore.Domain/StoredContext/Entities/OrderItem.cs
+++ CoreStore.Domain/StoredContext/Entities/OrderItem.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CoreStore.Domain.StoredContext
+namespace CoreStore.Domain.StoredContext.Entities
 {
     public class OrderItem
     {
--- CoreStore.Tests/UnitTest1.cs
+++ CoreStore.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using CoreStore.Domain.StoredContext.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CoreStore.Tests
@@ -8,6 +9,14 @@ namespace CoreStore.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            var c = new Customer("Michael",
+                "Muniz",
+                "12345678911",
+                "hello@michael.com",
+                "123456789",
+                "Rua dos Developers, 305" );
+
+
         }
     }
 }
