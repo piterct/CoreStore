721b66db5182e94fbb4776dbe1ddebb46764dad7
Michael Peter <michael_piterct@hotmail.com>
2020-01-30 18:57:16 -0300

    #19 Share Information with other Context

---
+++ CoreStore.Domain/BackOfficeContext/Entities/Customer.cs
@@ -0,0 +1,14 @@
+﻿using CoreStore.Domain.StoredContext.Entities;
+using CoreStore.Domain.StoredContext.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreStore.Domain.BackOfficeContext.Entities
+{
+    public class Customer
+    {
+        public IReadOnlyCollection<Product> Produts { get; set; }
+        public Name Name { get; set; }
+    }
+}
