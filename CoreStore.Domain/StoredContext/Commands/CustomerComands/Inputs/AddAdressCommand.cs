1e419d5ccc3e3eb24484b6a1661165b8f6885800
Michael Peter <michael_piterct@hotmail.com>
2020-01-31 17:08:16 -0300

    #24 Create First Command

---
+++ CoreStore.Domain/StoredContext/Commands/CustomerComands/Inputs/AddAdressCommand.cs
@@ -0,0 +1,21 @@
+﻿using CoreStore.Domain.StoredContext.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreStore.Domain.StoredContext.Commands.CustomerComands.Inputs
+{
+    public class AddAdressCommand
+    {
+        public Guid Id { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string Complement { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+        public EAddressType Type { get; set; }
+    }
+}
---
+++ CoreStore.Domain/StoredContext/Commands/CustomerComands/Inputs/CreateCustomerCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreStore.Domain.StoredContext.Commands.CustomerComands.Inputs
+{
+    public class CreateCustomerCommand
+    {
+        public string Firstname { get; set; }
+        public string LastName { get; set; }
+        public string Dcoument { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+    }
+}
