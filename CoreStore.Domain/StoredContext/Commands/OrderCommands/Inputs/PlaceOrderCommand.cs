cd2e6615d07767b579505fe522e1372988bfca09
Michael Peter <michael_piterct@hotmail.com>
2020-02-01 11:43:33 -0300

    #25 Create Other Commands

---
+++ CoreStore.Domain/StoredContext/Commands/OrderCommands/Inputs/PlaceOrderCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreStore.Domain.StoredContext.Commands.OrderCommands.Inputs
+{
+    public class PlaceOrderCommand
+    {
+        public Guid Customer { get; set; }
+        public IList<OrderItemCommand> OrderItems { get; set; }
+    }
+
+    public class OrderItemCommand
+    {
+        public Guid Product { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
