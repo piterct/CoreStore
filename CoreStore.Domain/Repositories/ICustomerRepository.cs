897bbcb3c2f2608bb444a7f80a9f106f1b70a385
Michael Peter <michael_piterct@hotmail.com>
2020-02-01 23:22:31 -0300

    #31 Repositories

---
+++ CoreStore.Domain/Repositories/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using CoreStore.Domain.StoredContext.Entities;
+
+namespace CoreStore.Domain.Repositories
+{
+    public interface  ICustomerRepository
+    {
+        bool CustomerDcoument(string document);
+        bool CheckEmail(string email);
+        void Save(Customer customer);
+    }
+}
