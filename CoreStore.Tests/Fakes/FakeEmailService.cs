e9a41c26eab3932742ce5881f9f0b8fa71a904b8
Michael Peter <michael_piterct@hotmail.com>
2020-02-02 00:13:17 -0300

    #34 Mocks and Fakes

---
+++ CoreStore.Tests/Fakes/FakeEmailService.cs
@@ -0,0 +1,13 @@
+﻿using CoreStore.Domain.StoredContext.Services;
+using System;
+
+namespace CoreStore.Tests.Fakes
+{
+    public class FakeEmailService : IEmailService
+    {
+        public void Send(string to, string from, string subject, string body)
+        {
+            
+        }
+    }
+}
---
+++ CoreStore.Tests/Handlers/CustomerHandlerTests.cs
@@ -0,0 +1,27 @@
+﻿using CoreStore.Domain.StoredContext.Commands.CustomerComands.Inputs;
+using CoreStore.Domain.StoredContext.Handlers;
+using CoreStore.Tests.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoreStore.Tests.Handlers
+{
+    [TestClass]
+    public class CustomerHandlerTests
+    {
+        [TestMethod]
+        public void ShouldRegisterCustomerWhenCommandIsValid()
+        {
+            var command = new CreateCustomerCommand();
+            command.FirstName = "Michael";
+            command.LastName = "Muniz";
+            command.Document = "27728919415";
+            command.Email = "michael.developer@gmail.com";
+            command.Phone = "11999999999";
+
+            Assert.AreEqual(true, command.Valid());
+
+            var handle = new CustomerHandler(new FakeCustomerRepository(), new FakeEmailService());
+        }
+
+    }
+}
