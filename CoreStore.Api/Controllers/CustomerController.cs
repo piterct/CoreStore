12c8b4717dcb02871893aa6c307cadd19e65d243
Michael Peter <michael_piterct@hotmail.com>
2020-02-03 20:54:34 -0300

    #46 Testing CRUD

--- CoreStore.Api/Controllers/CustomerController.cs
+++ CoreStore.Api/Controllers/CustomerController.cs
@@ -1,26 +1,38 @@
-﻿using CoreStore.Domain.StoredContext.Entities;
+﻿using CoreStore.Domain.StoredContext.Commands.CustomerComands.Inputs;
+using CoreStore.Domain.StoredContext.Entities;
+using CoreStore.Domain.StoredContext.ValueObjects;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CoreStore.Api.Controllers
 {
     public class CustomerController : Controller
     {
-        [HttpPost]
+        [HttpGet]
         [Route("customers")]
         public List<Customer> Get()
         {
-            return null;
+            var name = new Name("Michael", "Muniz");
+            var document = new Document("21950004066");
+            var email = new Email("michael.developer@gmail.com");
+            var customer = new Customer(name, document, email, "4521020312");
+            var customers = new List<Customer>();
+            customers.Add(customer);
+
+            return customers;
         }
 
         [HttpGet]
         [Route("customers/{id}")]
         public Customer GetById(Guid id)
         {
-            return null;
+            var name = new Name("Michael", "Muniz");
+            var document = new Document("21950004066");
+            var email = new Email("michael.developer@gmail.com");
+            var customer = new Customer(name, document, email, "4521020312");
+
+            return customer;
         }
 
 
@@ -28,28 +40,52 @@ namespace CoreStore.Api.Controllers
         [Route("customers/{id}/orders")]
         public List<Order> GetOrders(Guid id)
         {
-            return null;
+            var name = new Name("Michael", "Muniz");
+            var document = new Document("3221077076");
+            var email = new Email("michael.developer@gmail.com");
+            var customer = new Customer(name, document, email, "4521020312");
+
+            var order = new Order(customer);
+            var mouse = new Product("Mouse Gamer", "Mouse Gamer", "mouse.jpg", 100M, 10);
+            var monitor = new Product("Monitor Gamer", "Monitor Game", "monitor.jpg", 100M, 10);
+
+            order.AddItem(monitor, 5);
+            order.AddItem(mouse, 5);
+            var orders = new List<Order>();
+            orders.Add(order);
+
+            return orders;
         }
 
         [HttpPost]
         [Route("customers")]
-        public Customer Post([FromBody]Customer customer)
+        public Customer Post([FromBody]CreateCustomerCommand command)
         {
-            return null;
+            var name = new Name(command.FirstName, command.LastName);
+            var document = new Document(command.Document);
+            var email = new Email(command.Email);
+            var customer = new Customer(name, document, email, command.Phone);
+
+            return customer;
         }
 
         [HttpPut]
         [Route("customers/{id}")]
-        public Customer Put([FromBody]Customer customer)
+        public Customer Put([FromBody]CreateCustomerCommand command)
         {
-            return null;
+            var name = new Name(command.FirstName, command.LastName);
+            var document = new Document(command.Document);
+            var email = new Email(command.Email);
+            var customer = new Customer(name, document, email, command.Phone);
+
+            return customer;
         }
 
         [HttpDelete]
         [Route("customers/{id}")]
-        public Customer Delete(int id)
+        public object Delete()
         {
-            return null;
+            return new { message = "Cliente removido com sucesso!" };
         }
     }
 }
--- CoreStore.Api/Controllers/HomeController.cs
+++ CoreStore.Api/Controllers/HomeController.cs
@@ -4,8 +4,8 @@ namespace CoreStore.Api.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpPost]
-        [Route("rota/01")]
+        [HttpGet]
+        [Route("")]
         public object Get()
         {
             return new { version = "Version 0.0.1" };
