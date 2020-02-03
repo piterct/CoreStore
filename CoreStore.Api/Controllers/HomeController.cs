c096e698113abfd815db7e857b441af5af393942
Michael Peter <michael_piterct@hotmail.com>
2020-02-03 17:27:37 -0300

    #43 Create a Controller

---
+++ CoreStore.Api/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreStore.Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public object Get()
+        {
+            return new { version = "Version 0.0.1" };
+        }
+    }
+}
