3a899ef42a2321fd78d0bb2cbe3c20421c64b41e
Michael Peter <michael_piterct@hotmail.com>
2020-02-06 12:25:14 -0300

    #54 Document the API with Swagger

--- CoreStore.Api/Startup.cs
+++ CoreStore.Api/Startup.cs
@@ -6,8 +6,9 @@ using CoreStore.Infra.StoredContext.Repositories;
 using CoreStore.Infra.StoredContext.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace CoreStore.Api
 {
@@ -24,6 +25,17 @@ namespace CoreStore.Api
             services.AddTransient<ICustomerRepository, CustomerRepository>();
             services.AddTransient<IEmailService, EmailService>();
             services.AddTransient<CustomerHandler, CustomerHandler>();
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Core Store",
+                    Description = "API em .net core",
+                    Contact = new OpenApiContact() { Name = "CoreStore", Email = "michael.peter@developer.com.br"  }
+                });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -33,6 +45,12 @@ namespace CoreStore.Api
 
             app.UseMvc();
             app.UseResponseCompression();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(x =>
+            {
+                x.SwaggerEndpoint("/swagger/v1/swagger.json", "Core Store - V1");
+            });
         }
     }
 }
