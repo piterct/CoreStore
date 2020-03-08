f8678fbb5ca500bae5165cac38312fd3943aadc7
Michael Peter <michael_piterct@hotmail.com>
2020-03-08 00:52:32 -0300

    # Refactoring Order and Settings

--- CoreStore.Shared/Settings.cs
+++ CoreStore.Shared/Settings.cs
@@ -6,6 +6,8 @@ namespace CoreStore.Shared
 {
    public  class Settings
     {
-        public static string ConnectionString = @"Server=.;Database=CoreStore;User ID=sa;Password=Blite@2019;";
+        //public static string ConnectionString = @"Server=.;Database=CoreStore;User ID=sa;Password=Blite@2019;";
+        public static string ConnectionString = @"Server=.;Database=CoreStore;User ID=sa;Password=asking2asking2;";
+
     }
 }
