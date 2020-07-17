75b4b9477fad7222a44ba5cbcc5c43a38abfa4dc
Michael Peter <michael_piterct@hotmail.com>
2020-07-17 02:12:08 -0300

    Example Bulk Insert with Dapper

--- CoreStore.Infra/StoredContext/Repositories/CustomerRepository.cs
+++ CoreStore.Infra/StoredContext/Repositories/CustomerRepository.cs
@@ -2,22 +2,28 @@
 using CoreStore.Domain.StoredContext.Queries;
 using CoreStore.Domain.StoredContext.Repositories;
 using CoreStore.Infra.StoredContext.DataContexts;
+using CoreStore.Shared;
 using Dapper;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace CoreStore.Infra.StoredContext.Repositories
 {
     public class CustomerRepository : ICustomerRepository
     {
         private readonly CoreDataContext _context;
+        private readonly IOptions<Settings> _config;
 
-        public CustomerRepository(CoreDataContext context)
+        public CustomerRepository(CoreDataContext context, IOptions<Settings> config)
 
         {
             _context = context;
+            _config = config;
         }
         public bool CheckDocument(string document)
         {
@@ -108,6 +114,37 @@ namespace CoreStore.Infra.StoredContext.Repositories
                     Type = address.Type,
                 }, commandType: CommandType.StoredProcedure);
             }
+        }
+
+        public async Task InsertInBulk(IList<string> userNames)
+        {
+            var sqls = GetSqlsInBatches(userNames);
+            using (var connection = new SqlConnection(_config.Value.ConnectionString))
+            {
+                foreach (var sql in sqls)
+                {
+                    await connection.ExecuteAsync(sql);
+                }
+            }
+        }
+
+        private IList<string> GetSqlsInBatches(IList<string> userNames)
+        {
+            var insertSql = "INSERT INTO [Users] (Name, LastUpdatedAt) VALUES ";
+            var valuesSql = "('{0}', getdate())";
+            var batchSize = 1000;
+
+            var sqlsToExecute = new List<string>();
+            var numberOfBatches = (int)Math.Ceiling((double)userNames.Count / batchSize);
+
+            for (int i = 0; i < numberOfBatches; i++)
+            {
+                var userToInsert = userNames.Skip(i * batchSize).Take(batchSize);
+                var valuesToInsert = userToInsert.Select(u => string.Format(valuesSql, u));
+                sqlsToExecute.Add(insertSql + string.Join(",", valuesToInsert));
+            }
+
+            return sqlsToExecute;
         }
     }
 }
--- CoreStore.Shared/Settings.cs
+++ CoreStore.Shared/Settings.cs
@@ -4,10 +4,10 @@ using System.Text;
 
 namespace CoreStore.Shared
 {
-   public  class Settings
+    public class Settings
     {
         //public static string ConnectionString = @"Server=.;Database=CoreStore;User ID=sa;Password=Blite@2019;";
-        public static string ConnectionString = @"Server=.;Database=CoreStore;User ID=sa;Password=asking2asking2;";
+        public string ConnectionString = @"Server=.;Database=CoreStore;User ID=sa;Password=asking2asking2;";
 
     }
 }
